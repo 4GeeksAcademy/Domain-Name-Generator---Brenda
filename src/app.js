5a1538c20551bda702dc7ce490d21fec67f44e7d
Brenda Marquez <brenda.jmarq@gmail.com>
2024-07-02 01:52:19 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "raccoon"];
+
+  let domains = [];
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        domains.push(p + a + n + ".com");
+      }
+    }
+  }
+
+  for (let domain of domains) {
+    console.log(domain);
+  }
+
+  // Display the generated domain names on the web page
+  const container = document.createElement("div");
+  domains.forEach(domain => {
+    const domainElement = document.createElement("p");
+    domainElement.textContent = domain;
+    container.appendChild(domainElement);
+  });
+  document.body.appendChild(container);
 };
